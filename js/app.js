efec445a4bfb1ed8ef71827cd72d72bb50e3061a
DESKTOP-L8AHJ5I\Ashiq <ashiqul.eee@gmail.com>
2022-02-17 14:14:20 +0600

    some problem solve

--- js/app.js
+++ js/app.js
@@ -4,17 +4,10 @@ function catchInput(id, err) {
   const inputNumber = parseFloat(inputValue);
   if (inputNumber < 0) {
     document.getElementById(err).style.display = "block";
-    inputNumber = 0;
   } else {
     document.getElementById(err).style.display = "none";
     return inputNumber;
   }
-}
-function totalBalance() {
-  let income = catchInput("incomeInput", "error1");
-  let expenses = totalExpenses();
-  let totalbalance = income - expenses;
-  return totalbalance;
 }
 
 function totalExpenses() {
@@ -24,20 +17,33 @@ function totalExpenses() {
   let totalExpenses = food + rent + clothes;
   return totalExpenses;
 }
+function income() {
+  let income = catchInput("incomeInput", "error1");
+  return income;
+}
+
+function totalBalance() {
+  let expenses = totalExpenses();
+  let incomeTotal = income();
+  let totalbalance = incomeTotal - expenses;
+  return totalbalance;
+}
 
 const calculateButton = document
   .getElementById("calcButton")
   .addEventListener("click", function () {
-    const expenc = totalExpenses();
-    const balance = totalBalance();
-    if (expenc > balance) {
+    const incomeCall = income();
+    const expencCall = totalExpenses();
+    const balanceCall = totalBalance();
+    const expen = document.getElementById("totalExpen");
+    expen.innerText = expencCall;
+    const balanceElement = document.getElementById("balance");
+    if (expencCall > incomeCall) {
       document.getElementById("error5").style.display = "block";
+      balanceElement.innerText = 0;
     } else {
       document.getElementById("error5").style.display = "none";
-      const expen = document.getElementById("totalExpen");
-      expen.innerText = expenc;
-      const balanceElement = document.getElementById("balance");
-      balanceElement.innerText = balance;
+      balanceElement.innerText = balanceCall;
     }
   });
 
@@ -47,18 +53,19 @@ const saveButton = document
     const saveInput = document.getElementById("saveInput");
     const saveNumber = saveInput.value;
     const saveInputPercent = parseFloat(saveNumber);
+    const saving = document.getElementById("saving");
+    const remainBal = document.getElementById("remain");
     if (saveInputPercent > 100 || saveInputPercent < 0) {
       document.getElementById("error6").style.display = "block";
+      saving.innerText = 0;
+      remainBal.innerText = 0;
     } else {
       document.getElementById("error6").style.display = "none";
       let saveAmount = saveInputPercent / 100;
       let balanc = totalBalance();
       let totalSave = saveAmount * balanc;
       let remainBalance = balanc - totalSave;
-
-      const saving = document.getElementById("saving");
       saving.innerText = totalSave;
-      const remainBal = document.getElementById("remain");
       remainBal.innerText = remainBalance;
     }
   });
