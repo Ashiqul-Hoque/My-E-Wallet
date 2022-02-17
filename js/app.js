4dd3c7de89f66a47ec1472fa2fd9466f4ce3cc5a
DESKTOP-L8AHJ5I\Ashiq <ashiqul.eee@gmail.com>
2022-02-17 11:55:15 +0600

    calculation done

--- js/app.js
+++ js/app.js
@@ -1,24 +1,47 @@
-function catchInput(id, error) {
+function catchInput(id) {
   const inputAmount = document.getElementById(id);
   const inputValue = inputAmount.value;
   const inputNumber = parseFloat(inputValue);
-  if (inputNumber < 0) {
-    document.getElementById(error).style.display = "block";
-  }
   return inputNumber;
 }
-
-const claculateButton = document
+function totalExpenses() {
+  let food = catchInput("foodInput");
+  let rent = catchInput("rentInput");
+  let clothes = catchInput("clothesInput");
+  let totalExpenses = food + rent + clothes;
+  return totalExpenses;
+}
+function totalBalance() {
+  let income = catchInput("incomeInput");
+  let expenses = totalExpenses();
+  let totalbalance = income - expenses;
+  return totalbalance;
+}
+const calculateButton = document
   .getElementById("calcButton")
   .addEventListener("click", function () {
-    const income = catchInput("incomeInput", "error1");
-    const food = catchInput("foodInput", "error2");
-    const rent = catchInput("rentInput", "error3");
-    const clothes = catchInput("clothesInput", "error4");
-    let totalExpenses = food + rent + clothes;
-    const expen = document.getElementById("totalExpen");
-    expen.innerText = totalExpenses;
-    const balance = document.getElementById("balance");
-    const totalbalance = income - totalExpenses;
-    balance.innerText = totalbalance;
+    const expenc = totalExpenses();
+    const balance = totalBalance();
+    if (expenc > balance) {
+      document.getElementById("error5").style.display = "block";
+    } else {
+      const expen = document.getElementById("totalExpen");
+      expen.innerText = expenc;
+      const balanceElement = document.getElementById("balance");
+      balanceElement.innerText = balance;
+    }
+  });
+
+const saveButton = document
+  .getElementById("saveBtn")
+  .addEventListener("click", function () {
+    const saveInputPercent = catchInput("saveInput");
+    let saveAmount = saveInputPercent / 100;
+    let balanc = totalBalance();
+    let totalSave = saveAmount * balanc;
+    const saving = document.getElementById("saving");
+    saving.innerText = totalSave;
+    let remainBalance = balanc - totalSave;
+    const remainBal = document.getElementById("remain");
+    remainBal.innerText = remainBalance;
   });
